091348606c3bd97f062901dda2618c1662456e27
Blaise hala <blaisehala@gmail.com>
2024-06-09 22:52:05 +0300

    completes starter files setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Header from './components/Header'
+import Hero from './section/Hero'
+import Contact from './section/Contact'
+import Gallery from './section/Gallery'
+import Pricing from './section/Pricing'
+import Services from './section/Services'
+import Testimonial from './section/Testimonial'
+import Whychoose from './section/Whychoose'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header />
+      <Hero />
+      <Contact />
+      <Gallery />
+      <Pricing />
+      <Services />
+      <Testimonial />
+      <Whychoose />
+
+
+    </div>
   )
 }
 
